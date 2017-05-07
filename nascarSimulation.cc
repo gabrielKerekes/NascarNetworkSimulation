61b54d2af4a127d19222ba8c39cb0c26c18eee03
= <=>
2017-05-07 16:09:36 +0200

    everyone receives and sends - not working

--- nascarSimulation.cc
+++ nascarSimulation.cc
@@ -40,19 +40,35 @@ using namespace ns3;
 NS_LOG_COMPONENT_DEFINE("WirelessAnimationExample");
 
 uint32_t nWifi;
+uint32_t nObstacles;
 NodeContainer allNodes;
+NodeContainer passiveNodes;
 TypeId tid;
 Ipv4InterfaceContainer staInterfaces;
-    
+
 void ReceivePacket(Ptr<Socket> socket) {
     while (socket->Recv()) {
         NS_LOG_UNCOND("Received one packet!");
+        std::cout << "Packet received from id" << socket->GetNode()->GetId()  << std::endl;
     }
 }
 
 static void GenerateTraffic(Ptr<Socket> socket, uint32_t pktSize,
         uint32_t pktCount, Time pktInterval) {
-    // todo: num of nodes    
+
+    
+    if (pktCount > 0)
+    {
+        socket->Send (Create<Packet> (pktSize));
+        std::cout << "Packet sent from id" << socket->GetNode()->GetId() << std::endl;
+        Simulator::Schedule (pktInterval, &GenerateTraffic, 
+                socket, pktSize,pktCount-1, pktInterval);
+    }
+    else
+    {
+        socket->Close ();
+    }
+    
 //    int r = rand() % nWifi;
 //    int r2 = rand() % nWifi;
 //    
@@ -62,30 +78,39 @@ static void GenerateTraffic(Ptr<Socket> socket, uint32_t pktSize,
 //    
 //    if (pktCount > 0) {
 //        source->Send(Create<Packet> (pktSize));
+//        std::cout << "Packet sent from id" << source->GetNode()->GetId() << std::endl;
 //        Simulator::Schedule(pktInterval, &GenerateTraffic,
 //                socket, pktSize, pktCount - 1, pktInterval);
 //    }
-//    
-//    source->Close();
+//    else {
+//        source->Close();
+//    }
+    
 }
 
 int main(int argc, char *argv[]) 
 {
-    nWifi = 200;
+    nWifi = 20; // number of cars (12) + number of obstacles (8)
+    nObstacles = 8;
     double rss = -80; // -dBm
     uint32_t packetSize = 1000; // bytes
-    uint32_t numPackets = 1;
-    double interval = 1.0; // seconds
+    uint32_t numPackets = 50;
+    double interval = 1; // seconds
     
     srand(time(0));
-
+    
     CommandLine cmd;
-    cmd.AddValue("nWifi", "Number of wifi STA devices", nWifi);
+    cmd.AddValue("nWifi", "Number of Cars", nWifi);
+    cmd.AddValue("nObsacles", "Number of Obstacles", nObstacles);
     cmd.Parse(argc, argv);
-
+    
     NodeContainer wifiStaNodes;
     wifiStaNodes.Create(nWifi);
     allNodes.Add(wifiStaNodes);
+    
+    NodeContainer obstacleNodes;
+    obstacleNodes.Create(nObstacles);
+    passiveNodes.Add(obstacleNodes);
     
     std::string phyMode ("DsssRate1Mbps");
     
@@ -98,85 +123,143 @@ int main(int argc, char *argv[])
     wifiPhy.Set("RxGain", DoubleValue(0));
     // ns-3 supports RadioTap and Prism tracing extensions for 802.11b
     wifiPhy.SetPcapDataLinkType(YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
-
+    
     YansWifiChannelHelper wifiChannel;
     wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
     // The below FixedRssLossModel will cause the rss to be fixed regardless
     // of the distance between the two stations, and the transmit power
+    //wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel");
     wifiChannel.AddPropagationLoss("ns3::FixedRssLossModel", "Rss", DoubleValue(rss));
     wifiPhy.SetChannel(wifiChannel.Create());
-
+    
     // Add a mac and disable rate control
-    WifiMacHelper wifiMac;
+    NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
     wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
             "DataMode", StringValue(phyMode),
             "ControlMode", StringValue(phyMode));
     // Set it to adhoc mode
     wifiMac.SetType("ns3::AdhocWifiMac");
     NetDeviceContainer devices = wifi.Install (wifiPhy, wifiMac, wifiStaNodes);
-
+    
     ObjectFactory pos;
     pos.SetTypeId("ns3::RandomDiscPositionAllocator");
-    pos.Set("X", StringValue("50.0"));
-    pos.Set("Y", StringValue("50.0"));
+    pos.Set("X", StringValue("25.0"));
+    pos.Set("Y", StringValue("25.0"));
     pos.Set("Rho", StringValue("ns3::UniformRandomVariable[Min=0|Max=20]"));
-
+    
     Ptr<PositionAllocator> taPositionAlloc = pos.Create ()->GetObject<PositionAllocator> ();
     
-    // Mobility
-
+    // Mobility Cars
+    
     MobilityHelper mobility;
     mobility.SetPositionAllocator("ns3::RandomDiscPositionAllocator",
-            "X", StringValue("50.0"),
-            "Y", StringValue("50.0"),
+            "X", StringValue("25.0"),
+            "Y", StringValue("25.0"),
             "Rho", StringValue("ns3::UniformRandomVariable[Min=0|Max=20]"));
     
     mobility.SetMobilityModel("ns3::RandomWaypointMobilityModel",
             "Speed", StringValue("ns3::UniformRandomVariable[Min=10|Max=20]"),
             "Pause", StringValue("ns3::ConstantRandomVariable[Constant=0]"),
             "PositionAllocator", PointerValue(taPositionAlloc));
-
-//    mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
-//            "Bounds", RectangleValue(Rectangle(30, 70, 30, 70)),
-//            "Distance", DoubleValue(30),
-//            "Speed", StringValue("ns3::UniformRandomVariable[Min=5|Max=20]"));
     
     mobility.Install(wifiStaNodes);
-
+    
+    // Mobility Obstacles
+    
+    MobilityHelper mo;
+    mo.SetPositionAllocator ("ns3::GridPositionAllocator",//vzdialenost
+            "MinX", DoubleValue (0.0),
+            "MinY", DoubleValue (0.0),
+            "DeltaX", DoubleValue (10),
+            "DeltaY", DoubleValue (10),
+            "GridWidth", UintegerValue (3),
+            "LayoutType", StringValue ("RowFirst"));
+    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+    mobility.Install (obstacleNodes);
+    
+    // Internet
+    
     InternetStackHelper stack;
     stack.Install(allNodes);
-
+    
     Ipv4AddressHelper address;
     address.SetBase("10.1.1.0", "255.255.255.0");
     staInterfaces = address.Assign(devices);
     
+    //int randomReceive = rand() % nWifi;
+    
     tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-    Ptr<Socket> recvSink = Socket::CreateSocket(allNodes.Get(0), tid);
-    InetSocketAddress local = InetSocketAddress(Ipv4Address::GetAny(), 80);
-    recvSink->Bind(local);
-    recvSink->SetRecvCallback(MakeCallback(&ReceivePacket));
-
-    Ptr<Socket> source = Socket::CreateSocket(allNodes.Get(nWifi - 1), tid);
-    InetSocketAddress remote = InetSocketAddress(staInterfaces.GetAddress(0, 0), 80);
-    source->Connect(remote);
-    // Tracing
+    
+    
+    Ptr<Socket> recvSink[12];
+    for (uint32_t i = 0; i < 12; ++i) {
+        recvSink[i] = Socket::CreateSocket(allNodes.Get(i), tid);
+        InetSocketAddress local = InetSocketAddress(Ipv4Address::GetAny(), 80);
+        recvSink[i]->Bind(local);
+        recvSink[i]->SetRecvCallback(MakeCallback(&ReceivePacket));
+    }
+   
+    
+    Ptr<Socket> source[12];
+    
+    for (uint32_t i = 0; i < 12; ++i) {
+        source[i] = Socket::CreateSocket(allNodes.Get(i), tid);
+        InetSocketAddress remote = InetSocketAddress(staInterfaces.GetAddress(0, 0), 80);
+        source[i]->Connect(remote);
+    }
+    
+    // Tracing off?
     wifiPhy.EnablePcap ("wifi-simple-adhoc", devices);
-
-    double duration = 120.0;
+    
+    double duration = 60.0;
     
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
-    Simulator::Stop(Seconds(duration));
-
-    AnimationInterface anim("nascar-animation.xml"); // Mandatory
-    for (uint32_t i = 0; i < wifiStaNodes.GetN(); ++i) {
-        anim.UpdateNodeDescription(wifiStaNodes.Get(i), "STA"); // Optional
-        anim.UpdateNodeColor(wifiStaNodes.Get(i), 255, 0, 0); // Optional
+    Time interPacketInterval = Seconds (interval);
+    
+    for (uint32_t i = 0; i < 12; ++i) {
+        Simulator::ScheduleWithContext (source[i]->GetNode ()->GetId (),
+            Seconds (0.0),
+            &GenerateTraffic, 
+            source[i],
+            packetSize,
+            numPackets, // number of packets
+            interPacketInterval);
     }
+    
 
+    Simulator::Stop(Seconds(duration));
+    
+    // NetAnim
+    AnimationInterface anim("nascar.xml"); // Mandatory
+    
+    for (uint32_t i = 0; i < wifiStaNodes.GetN(); ++i) {
+        //anim.UpdateNodeDescription(wifiStaNodes.Get(i), "CAR"); // Optional
+        if (i % 4 == 0) {
+            uint32_t carImgIdGreen = anim.AddResource("/home/student/Documents/ns3/ns-3.24.1/greencar.png");
+            anim.UpdateNodeImage(i, carImgIdGreen);
+        } else if (i % 4 == 1) {
+            uint32_t carImgIdPurple = anim.AddResource("/home/student/Documents/ns3/ns-3.24.1/purplecar.png");
+            anim.UpdateNodeImage(i, carImgIdPurple);
+        } else if (i % 4 == 2) {
+            uint32_t carImgIdBlue = anim.AddResource("/home/student/Documents/ns3/ns-3.24.1/bluecar.png");
+            anim.UpdateNodeImage(i, carImgIdBlue);
+        } else {
+            uint32_t carImgIdOrange = anim.AddResource("/home/student/Documents/ns3/ns-3.24.1/orangecar.png");
+            anim.UpdateNodeImage(i, carImgIdOrange);
+        }
+        //anim.UpdateNodeColor(wifiStaNodes.Get(i), 255, 0, 0); // Optional
+        anim.UpdateNodeSize(i, 6, 1); 
+    }
+    for (uint32_t i = 0; i < obstacleNodes.GetN(); ++i) {
+        //anim.UpdateNodeDescription(obstacleNodes.Get(i), "Obstacle"); // Optional
+        anim.UpdateNodeColor(obstacleNodes.Get(i), 0, 0, 0); // Optional black
+        anim.UpdateNodeSize(i, 0.5, 0.5);
+    }
+    
     //g: set special name for node 0
-    anim.UpdateNodeDescription(wifiStaNodes.Get(0), "Boss"); // Optional
-    anim.UpdateNodeColor(wifiStaNodes.Get(0), 0, 123, 123); // Optional
-
+    anim.UpdateNodeDescription(wifiStaNodes.Get(0), "Master"); // Optional
+    //anim.UpdateNodeColor(wifiStaNodes.Get(0), 0, 123, 123); // Optional
+    
     UdpEchoClientHelper echoClient(staInterfaces.GetAddress(1), 9);
     echoClient.SetAttribute("MaxPackets", UintegerValue(1));
     echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
@@ -185,18 +268,12 @@ int main(int argc, char *argv[])
     ApplicationContainer clientApps = echoClient.Install(allNodes.Get(0));
     clientApps.Start(Seconds(0.0));
     clientApps.Stop(Seconds(duration));    
-
+    
     anim.EnablePacketMetadata(); // Optional
     anim.EnableIpv4RouteTracking("routingtable-wireless.xml", Seconds(0), Seconds(5), Seconds(0.25)); //Optional
     anim.EnableWifiMacCounters(Seconds(0), Seconds(duration)); //Optional
     anim.EnableWifiPhyCounters(Seconds(0), Seconds(duration)); //Optional
     
-    Time interPacketInterval = Seconds (interval);
-    
-    Simulator::ScheduleWithContext (source->GetNode ()->GetId (),
-                                  Seconds (1.0), &GenerateTraffic, 
-                                  source, packetSize, 69871, interPacketInterval);
-  
     Simulator::Run();
     Simulator::Destroy();
     
